c2e20e11b1b5c7f01834860dd011a6c300d2eefe
Manfred <d029940@users.noreply.github.com>
2024-02-06 18:16:12 +0100

    Infos for creating windows app

--- DeployWin.bat
+++ DeployWin.bat
@@ -1,4 +1,4 @@
 
 @echo on
-\Dev\Qt\6.4.1\mingw_64\bin\qtenv2.bat
-\Dev\Qt\6.4.1\mingw_64\bin\windeployqt.exe \Users\Manfred\Developer\CPlusPlus\Qt\Projects\build-AnalyzeGPX-Desktop_Qt_6_4_1_MinGW_64_bit-Release\apps\widgets
+\Dev\Qt\6.6.1\mingw_64\bin\qtenv2.bat
+\Dev\Qt\6.6.1\mingw_64\bin\windeployqt.exe \Users\Manfred\Developer\CPlusPlus\Projects\build-AnalyzeGPX-Desktop_Qt_6_6_1_MinGW_64_bit-Release\apps\widgets
